fafcd6be6e9cb0c82f4e42ad7a7e306b445a2954
山本晃司 <koji.19@icloud.com>
2025-10-10 10:23:18 +0900

    kadai_breezeプッシュ

--- resources/views/get.blade.php
+++ resources/views/get.blade.php
@@ -8,24 +8,44 @@
 </head>
 
 <body>
-   <table>
-       <tr>
-           <th>ID</th>
-           <th>商品名</th>
-           <th>内容</th>
-           <th>作成日時</th>
-           <th>更新日時</th>
-       </tr>
-       @foreach($posts as $post)
-           <tr>
-               <td>{{ $post->id }}</td>
-               <td>{{ $post->title }}</td>
-               <td>{{ $post->content }}</td>
-               <td>{{ $post->created_at }}</td>
-               <td>{{ $post->updated_at }}</td>
-           </tr>            
-       @endforeach       
-   </table>
+    <style>
+        div{
+            text-align: center;
+        }
+        .btn{
+            background-color: blue;
+            color: #fff;
+            font-size: 1.2rem;
+            font-weight: 600;
+            border-radius: 8px;
+            border: none;
+            cursor: pointer; 
+        }
+    </style>
+<div>
+    <form action="{{ route('posts.create') }}" method="get">
+        @csrf
+        <input type="submit" value="投稿作成" class="btn">
+    </form>
+</div>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>商品名</th>
+            <th>内容</th>
+            <th>作成日時</th>
+            <th>更新日時</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->content }}</td>
+                <td>{{ $post->created_at }}</td>
+                <td>{{ $post->updated_at }}</td>
+            </tr>            
+        @endforeach       
+    </table>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +20,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+//以下、教材or課題進行
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
 
-Route::get('/posts/create', [PostsController::class, 'create']);
-Route::post('/posts/create', [PostsController::class, 'store'])->name('posts.store');
-
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::post('/posts/create', [PostsController::class, 'store'])->name('posts.store');
+});
 
 Route::get('/posts/{id}', [PostsController::class, 'show']);
 
